555feee9bc55fa172b994bffbf623370495a8c46
Huy Tran <huytran21007@gmail.com>
2023-04-21 18:24:03 +0700

    test login page v3

--- src/Pages/Login/LoginPage.jsx
+++ src/Pages/Login/LoginPage.jsx
@@ -14,73 +14,79 @@ import { checkAllInfo } from "../../core/utils/checkLogin";
 
 const LoginPage = () => {
   const navigate = useNavigate();
-
-  useEffect(() => {
-    if (LOCAL_SERVICE.user.get()) {
-      navigate("/");
-    }
-  }, []);
-
-  const handleFinish = (values, buttonRef) => {
-    checkAllInfo(values)
-      .then((res) => {
-        if (!Object.keys(res).length) {
-          CustomNotification(
-            "error",
-            "Login fails",
-            "Please check your login info again"
-          );
-          throw new Error("Fail!!!");
-        }
-        buttonRef.current.disabled = true;
-
-        return res;
-      })
-      .then((res) => {
-        CustomNotification("success", "Login ok", "Please wait a minute");
-        setTimeout(() => {
-          navigate("/");
-          if (res.hasOwnProperty("tasks")) {
-            LOCAL_SERVICE.user.set(res, res.role);
-          } else {
-            LOCAL_SERVICE.user.set({ ...res, tasks: [] }, res.role);
-          }
-        }, 2500);
-      })
-      .catch((error) => {
-        console.log("error");
-        console.log(error);
-      });
-  };
-
   const renderPage = () => {
     return (
-      <PageWrapper className="page-login h-full">
-        <Container className="h-full">
-          <div className="wrapper flex items-center justify-center h-full">
-            <Space
-              className="form-wrapper bg-white rounded-[15px] p-7 max-w-[500px] w-full"
-              align="center"
-              direction="vertical"
-              size={20}
-            >
-              <div className="form-header text-center w-full">
-                <Link to="/" className="pb-8 flex items-center justify-center">
-                  <img src={logoPage} alt="logo-page" className="logo" />
-                </Link>
-                <h3 className="form-title border-t border-solid border-[#EBF1FF] pt-8 text-xl font-semibold mb-0">
-                  Login
-                </h3>
-              </div>
-              <div className="form-body">
-                <LoginForm handleFinish={handleFinish} />
-              </div>
-            </Space>
-          </div>
-        </Container>
-      </PageWrapper>
+      <div className="cursor-pointer bg-red-700 text-white">
+        Testing login page
+      </div>
     );
   };
+  // useEffect(() => {
+  //   if (LOCAL_SERVICE.user.get()) {
+  //     navigate("/");
+  //   }
+  // }, []);
+
+  // const handleFinish = (values, buttonRef) => {
+  //   checkAllInfo(values)
+  //     .then((res) => {
+  //       if (!Object.keys(res).length) {
+  //         CustomNotification(
+  //           "error",
+  //           "Login fails",
+  //           "Please check your login info again"
+  //         );
+  //         throw new Error("Fail!!!");
+  //       }
+  //       buttonRef.current.disabled = true;
+
+  //       return res;
+  //     })
+  //     .then((res) => {
+  //       CustomNotification("success", "Login ok", "Please wait a minute");
+  //       setTimeout(() => {
+  //         navigate("/");
+  //         if (res.hasOwnProperty("tasks")) {
+  //           LOCAL_SERVICE.user.set(res, res.role);
+  //         } else {
+  //           LOCAL_SERVICE.user.set({ ...res, tasks: [] }, res.role);
+  //         }
+  //       }, 2500);
+  //     })
+  //     .catch((error) => {
+  //       console.log("error");
+  //       console.log(error);
+  //     });
+  // };
+
+  // const renderPage = () => {
+  //   return (
+  //     <PageWrapper className="page-login h-full">
+  //       <Container className="h-full">
+  //         <div className="wrapper flex items-center justify-center h-full">
+  //           <Space
+  //             className="form-wrapper bg-white rounded-[15px] p-7 max-w-[500px] w-full"
+  //             align="center"
+  //             direction="vertical"
+  //             size={20}
+  //           >
+  //             <div className="form-header text-center w-full">
+  //               <Link to="/" className="pb-8 flex items-center justify-center">
+  //                 <img src={logoPage} alt="logo-page" className="logo" />
+  //               </Link>
+  //               <h3 className="form-title border-t border-solid border-[#EBF1FF] pt-8 text-xl font-semibold mb-0">
+  //                 Login
+  //               </h3>
+  //             </div>
+  //             <div className="form-body">
+  //               <LoginForm handleFinish={handleFinish} />
+  //             </div>
+  //           </Space>
+  //         </div>
+  //       </Container>
+  //     </PageWrapper>
+  //   );
+  // };
 
   return renderPage();
 };
