4a2cb9a324bbe861f1fc2e5c1ca412d9dd1f8c2d
Huy Tran <huytran21007@gmail.com>
2023-04-21 19:58:48 +0700

    Test no providoer

--- src/index.js
+++ src/index.js
@@ -18,11 +18,11 @@ import "./core/assets/styles/_table.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    {/* <BrowserRouter> */}
+  // <Provider store={store}>
+  <BrowserRouter>
     <App />
-    {/* </BrowserRouter> */}
-  </Provider>
+  </BrowserRouter>
+  // </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
